92f0dbe612f2746f482a4eb7e8dadbb618bd7175
Kushang Shah <kushangshah131@gmail.com>
2020-09-08 17:58:01 +0530

    Update addquestion.php

--- Ads/Min/Subs/addquestion.php
+++ Ads/Min/Subs/addquestion.php
@@ -5,6 +5,7 @@
 		$data = trim($data);
 		$data = stripslashes($data);
 		$data = htmlspecialchars($data);
+		$data = addslashes($data);
 		return $data;
 	}
 
@@ -29,13 +30,12 @@
 		
 		require_once("../../AdminDb/dbconnect.php");
 		
-		$query = "INSERT INTO `questions`(q_number, q_data, q_op1, q_op2, q_op3, q_op4, q_answer, q_hint, q_data_guj, q_op1_guj, q_op2_guj, q_op3_guj, q_op4_guj, q_answer_guj, q_hint_guj, q_sub) VALUES($no, '$q_data_e', '$q_op1_e', '$q_op2_e', '$q_op3_e', '$q_op4_e', '$q_ans_e', '$q_hint_e', '$q_data_g', '$q_op1_g', '$q_op2_g', '$q_op3_g', '$q_op4_g', '$q_ans_g', '$q_hint_g', $q_type)";
+		$query = "INSERT INTO `questions`(q_number, q_data, q_op1, q_op2, q_op3, q_op4, q_answer, q_hint, q_data_guj, q_op1_guj, q_op2_guj, q_op3_guj, q_op4_guj, q_answer_guj, q_hint_guj, q_sub, q_img ,hh) VALUES($no, '$q_data_e', '$q_op1_e', '$q_op2_e', '$q_op3_e', '$q_op4_e', '$q_ans_e', '$q_hint_e', '$q_data_g', '$q_op1_g', '$q_op2_g', '$q_op3_g', '$q_op4_g', '$q_ans_g', '$q_hint_g', $q_type, 'kgblk', 1)";
 		
 		if($conn->query($query)){
 		    
 		}
 		else{
-		    echo $query;
 		    echo "Something went wrong";
 		}
 		
