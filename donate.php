bf2f77e15f60de0cbb60b2cac7f2d2af04333977
Kushang Shah <kushangshah131@gmail.com>
2020-09-12 09:08:40 +0530

    Create donate.php

---
+++ donate.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Donate | NEET demo test</title>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="icon" href="https://keltaking.co/favicon.ico" type="image/gif" sizes="20x20"> 
+        <link href="https://keltaking.co/favicon.ico" rel="apple-touch-icon" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Bree Serif' rel='stylesheet'>
+        <link rel="stylesheet" href="CSS/kel.css">
+        <style>
+            .fonts {
+                font-family: 'Bree Serif';font-size: 22px;
+            }
+            .loader{
+                border: 5px solid #f3f3f3;
+              border-radius: 50%;
+              border-top: 5px solid #3498db;
+              width: 30px;
+              height: 30px;
+              -webkit-animation: spin 1s linear infinite; /* Safari */
+              animation: spin 1s linear infinite;
+            }
+            
+        </style>
+	</head>
+	<body class="">
+	    
+<div class="w3-theme">
+    <div class="w3-content">
+    <div class="w3-bar w3-padding" id="myNavbar">
+    <a href="login" class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large kel-hover w3-white"><i class="fa fa-sign-in"></i> Log in</a>
+    <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right kel-hover" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
+      <i class="fa fa-bars"></i>
+    </a>
+    <a href="/Exams" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round w3-right"><i class="fa fa-list"></i> Register</a>
+    <a href="results" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round"><i class="fa fa-th"></i> Results</a>
+    <a href="contact" class="w3-bar-item w3-button w3-hide-small kel-hover w3-hover-theme-dark w3-round w3-right"><i class="fa fa-envelope"></i> Contact</a>
+    <a href="login" class="w3-bar-item w3-button w3-hide-small kel-hover w3-round w3-right w3-white w3-hover-theme-dark"><i class="fa fa-sign-in"></i> Log in</a>
+    </div>
+    
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-animate-top">
+    <a href="/Exams" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover" onclick="toggleFunction()"><i class="fa fa-list"></i> Register</a>
+    <a href="results" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover" onclick="toggleFunction()"><i class="fa fa-th"></i> Results</a>
+    <a href="contact" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover" onclick="toggleFunction()"><i class="fa fa-envelope"></i> Contact</a>
+    <a href="login" class="w3-bar-item w3-button w3-theme-l1 w3-hover-theme-dark kel-hover" onclick="toggleFunction()"><i class="fa fa-sign-in"></i> Log in</a>
+    
+  </div>
+</div>
+</div>
+
+<div class="w3-center">
+    <img class="w3-image" src="Images/underconstruction.png">
+</div>
+
+<script src="JS/donate.js"></script>
+	</body>
+</html>
